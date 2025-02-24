112aa7ddea4b33a934239a698964c0dca4458f25
Addison Alvey-Blanco <aja13@illinois.edu>
2025-02-24 14:47:37 -0600

    start outlining new functionality

--- namedisl/__init__.py
+++ namedisl/__init__.py
@@ -36,7 +36,9 @@ import re
 from collections.abc import Mapping
 from dataclasses import dataclass
 from importlib import metadata
-from typing import TypeAlias, TypeVar, overload
+from typing import Callable, Sequence, TypeAlias, TypeVar, overload
+
+import operator
 
 from constantdict import constantdict
 
@@ -48,9 +50,26 @@ _match = re.match(r"^([0-9.]+)([a-z0-9]*?)$", __version__)
 assert _match
 VERSION = tuple(int(nr) for nr in _match.group(1).split("."))
 
+
+# {{{ typing
+
 IslObject = TypeVar("IslObject", isl.BasicSet, isl.Set, isl.BasicMap, isl.Map)
 NameToDim: TypeAlias = Mapping[str, tuple[isl.dim_type, int]]
 
+@dataclass(frozen=True)
+class NamedIslObject:
+    _obj: IslObject
+    _name_to_dim: NameToDim
+
+    def __str__(self) -> str:
+        return str(_restore_names(self._obj, self._name_to_dim))
+
+NamedBinaryOp = Callable[[NamedIslObject, NamedIslObject], NamedIslObject]
+
+# }}}
+
+
+# {{{ utils
 
 def _strip_names(obj: IslObject) -> tuple[IslObject, NameToDim]:
     name_to_dim = {}
@@ -75,19 +94,51 @@ def _restore_names(obj: IslObject, name_to_dim: NameToDim) -> IslObject:
 
     return obj
 
+
+def _find_alignment_ordering(obj: NamedIslObject,
+                             template: NamedIslObject) -> Sequence:
+    ordering = []
+
+    return ordering
+
+
+def _apply_ordering(obj: NamedIslObject,
+                    ordering: Sequence) -> NamedIslObject:
+    return obj
+
+
+def align_spaces(obj: NamedIslObject,
+                 template: NamedIslObject,
+                 obj_bigger_ok: bool = False) -> NamedIslObject:
+    return _apply_ordering(obj, _find_alignment_ordering(obj, template))
+
+
+def align_two(obj1: NamedIslObject,
+              obj2: NamedIslObject) -> Sequence[NamedIslObject]:
+    obj1 = align_spaces(obj1, obj2, obj_bigger_ok=True)
+    obj2 = align_spaces(obj2, obj1, obj_bigger_ok=True)
+    return obj1, obj2
+
+
+def align_and_apply_op(
+        obj1: NamedIslObject,
+        obj2: NamedIslObject,
+        op: Callable[[IslObject, IslObject], IslObject]) -> NamedIslObject:
+    obj1, obj2 = align_two(obj1, obj2)
+    result = op(obj1._obj, obj2._obj)
+    return type(obj1)(result, obj1._name_to_dim)
+
+# }}}
+
+
 # {{{ sets
 
 @dataclass(frozen=True)
-class BasicSet:
+class BasicSet(NamedIslObject):
     _obj: isl.BasicSet
-    _name_to_dim: NameToDim
 
     def __and__(self, other) -> BasicSet:
-        if self._name_to_dim == other._name_to_dim:
-            return BasicSet(self._obj & other._obj, self._name_to_dim)
-        else: # determine whether sets can be intersected
-            pass
-        raise ValueError("Name mismatch, cannot intersect sets")
+        return align_and_apply_op(self, other, operator.and_)
 
     def __str__(self) -> str:
         return str(_restore_names(self._obj, self._name_to_dim))
