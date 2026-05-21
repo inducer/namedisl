c1c8424c6b920794017487a1fae331e7a944189a
Addison <aja13@illinois.edu>
2026-05-21 14:29:17 -0500

    configure docs

--- doc/conf.py
+++ doc/conf.py
@@ -3,18 +3,25 @@ from __future__ import annotations
 import sys
 from importlib import metadata
 from pathlib import Path
+from urllib.request import urlopen
 
 
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
+_conf_url = "https://tiker.net/sphinxconfig-v0.py"
+with urlopen(_conf_url) as _inf:
+    exec(compile(_inf.read(), _conf_url, "exec"), globals())
+
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
+    "sphinx.ext.linkcode",
 ]
 
 autodoc_member_order = "bysource"
 autodoc_typehints = "none"
 
+project = "namedisl"
 copyright = "2025- University of Illinois Board of Trustees"
 author = "Andreas Kloeckner"
 
